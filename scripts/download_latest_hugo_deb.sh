0e64418b1758cb8f15b9ee2d94286251170ba0e8
Hong Xu <hong@8hobbies.com>
2024-04-21 20:05:53 -0700

    Download the deb file to a subdir

--- scripts/download_latest_hugo_deb.sh
+++ scripts/download_latest_hugo_deb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 latest_hugo_version=$(git ls-remote --tags --sort='v:refname' https://github.com/gohugoio/hugo | tail -1 | cut -d '/' -f 3)
-wget -q -P _site "https://github.com/gohugoio/hugo/releases/download/${latest_hugo_version}/hugo_extended_${latest_hugo_version:1}_linux-amd64.deb"
+PACKAGE_DIR=_site/dists/latest
+mkdir -p "${PACKAGE_DIR}/binary-amd64"
+wget -q -P "${PACKAGE_DIR}/binary-amd64" "https://github.com/gohugoio/hugo/releases/download/${latest_hugo_version}/hugo_extended_${latest_hugo_version:1}_linux-amd64.deb"

cfb1a592d96ae45fabf9509add0bc08aad45f817
Hong Xu <hong@8hobbies.com>
2024-04-21 21:22:35 -0700

    Add informational fields to Release

--- scripts/generate_repo_files.sh
+++ scripts/generate_repo_files.sh
@@ -9,4 +9,10 @@ apt-ftparchive packages . > Packages
 gzip -k --best Packages
 cd ../..
 
-apt-ftparchive release . > Release
+apt-ftparchive \
+    -o APT::FTPArchive::Release::Origin="8 Hobbies" \
+    -o APT::FTPArchive::Release::Suite="latest" \
+    -o APT::FTPArchive::Release::Version="1.0" \
+    -o APT::FTPArchive::Release::Architectures="amd64" \
+    -o APT::FTPArchive::Release::Components="main" \
+    release . > Release

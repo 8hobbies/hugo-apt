1f93309ce78b90c35d14620ef339eaaadf7f56b2
Hong Xu <hong@8hobbies.com>
2024-04-21 20:27:14 -0700

    Generate Debian repo files

---
+++ scripts/generate_repo_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Generate Packages.gz and Release files.
+
+cd ${DIST_ROOT_DIR}
+
+cd binary-amd64
+apt-ftparchive packages . | gzip --best -c > Packages.gz
+cd ..
+
+apt-ftparchive release . > Release

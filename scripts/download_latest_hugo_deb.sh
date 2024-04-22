c747564e6e7bf6f3ca1df164dc07b07719df2aec
Hong Xu <hong@8hobbies.com>
2024-04-21 23:59:19 -0700

    Also support arm64 (#2)

--- scripts/download_latest_hugo_deb.sh
+++ scripts/download_latest_hugo_deb.sh
@@ -3,5 +3,7 @@
 set -e
 
 latest_hugo_version=$(git ls-remote --tags --sort='v:refname' https://github.com/gohugoio/hugo | tail -1 | cut -d '/' -f 3)
-mkdir -p "${DIST_ROOT_DIR}/main/binary-amd64"
-wget -q -P "${DIST_ROOT_DIR}/main/binary-amd64" "https://github.com/gohugoio/hugo/releases/download/${latest_hugo_version}/hugo_extended_${latest_hugo_version:1}_linux-amd64.deb"
+for arch in amd64 arm64; do
+    mkdir -p "${DIST_ROOT_DIR}/main/binary-${arch}"
+    wget -q -P "${DIST_ROOT_DIR}/main/binary-${arch}" "https://github.com/gohugoio/hugo/releases/download/${latest_hugo_version}/hugo_extended_${latest_hugo_version:1}_linux-${arch}.deb"
+done
--- scripts/generate_repo_files.sh
+++ scripts/generate_repo_files.sh
@@ -5,15 +5,17 @@
 set -e
 
 cd _site
-apt-ftparchive packages "${DIST_ROOT_DIR#*/}/main/binary-amd64" > "${DIST_ROOT_DIR#*/}/main/binary-amd64/Packages"
-gzip -k --best "${DIST_ROOT_DIR#*/}/main/binary-amd64/Packages"
+for arch in amd64 arm64; do
+    apt-ftparchive packages "${DIST_ROOT_DIR#*/}/main/binary-${arch}" > "${DIST_ROOT_DIR#*/}/main/binary-${arch}/Packages"
+    gzip -k --best "${DIST_ROOT_DIR#*/}/main/binary-${arch}/Packages"
+done
 
-cd "../${DIST_ROOT_DIR}"
+cd "${DIST_ROOT_DIR#*/}"
 
 apt-ftparchive \
     -o APT::FTPArchive::Release::Origin="8 Hobbies" \
     -o APT::FTPArchive::Release::Suite="latest" \
     -o APT::FTPArchive::Release::Version="1.0" \
-    -o APT::FTPArchive::Release::Architectures="amd64" \
+    -o APT::FTPArchive::Release::Architectures="amd64,arm64" \
     -o APT::FTPArchive::Release::Components="main" \
     release . > Release

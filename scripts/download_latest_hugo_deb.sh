5f18bcc7523e3c115e1b93df8d20e993235c3841
Hong Xu <hong@8hobbies.com>
2024-04-22 01:36:48 -0700

    Check the checksum after downloading the hugo deb file (#8)

--- scripts/download_latest_hugo_deb.sh
+++ scripts/download_latest_hugo_deb.sh
@@ -3,7 +3,17 @@
 set -e
 
 latest_hugo_version=$(git ls-remote --tags --sort='v:refname' https://github.com/gohugoio/hugo | tail -1 | cut -d '/' -f 3)
+
+# Download checksum file
+wget -q "https://github.com/gohugoio/hugo/releases/download/${latest_hugo_version}/hugo_${latest_hugo_version:1}_checksums.txt" -O checksums.txt
+
+# Download each arch and verify checksum
 for arch in amd64 arm64; do
-    mkdir -p "${DIST_ROOT_DIR}/main/binary-${arch}"
-    wget -q -P "${DIST_ROOT_DIR}/main/binary-${arch}" "https://github.com/gohugoio/hugo/releases/download/${latest_hugo_version}/hugo_extended_${latest_hugo_version:1}_linux-${arch}.deb"
+    save_dir="${DIST_ROOT_DIR}/main/binary-${arch}"
+    hugo_deb_file_name="hugo_extended_${latest_hugo_version:1}_linux-${arch}.deb"
+    mkdir -p "${save_dir}"
+    wget -q -P "${save_dir}" "https://github.com/gohugoio/hugo/releases/download/${latest_hugo_version}/${hugo_deb_file_name}"
+    checksum_line="$(grep "${hugo_deb_file_name}" checksums.txt | cut -f 1 -d ' ') ${save_dir}/${hugo_deb_file_name}"
+    echo $checksum_line
+    sha256sum --check - <<< "$checksum_line"
 done

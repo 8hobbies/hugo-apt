1802a8d8354e0c9b42b38f8ba982769a6ee38207
Hong Xu <hong@8hobbies.com>
2024-04-21 20:10:26 -0700

    Use DIST_ROOT_DIR environment variable

--- scripts/download_latest_hugo_deb.sh
+++ scripts/download_latest_hugo_deb.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 latest_hugo_version=$(git ls-remote --tags --sort='v:refname' https://github.com/gohugoio/hugo | tail -1 | cut -d '/' -f 3)
-PACKAGE_DIR=_site/dists/latest
-mkdir -p "${PACKAGE_DIR}/binary-amd64"
-wget -q -P "${PACKAGE_DIR}/binary-amd64" "https://github.com/gohugoio/hugo/releases/download/${latest_hugo_version}/hugo_extended_${latest_hugo_version:1}_linux-amd64.deb"
+mkdir -p "${DIST_ROOT_DIR}/binary-amd64"
+wget -q -P "${DIST_ROOT_DIR}/binary-amd64" "https://github.com/gohugoio/hugo/releases/download/${latest_hugo_version}/hugo_extended_${latest_hugo_version:1}_linux-amd64.deb"
